dc62926cda63700a66871f0bf12d616f42948010
AFPedreros <felipe.pedreros94@gmail.com>
2022-08-02 12:33:16 -0500

    5. Token transfers

--- test/Token.js
+++ test/Token.js
@@ -10,15 +10,19 @@ describe("Token", () => {
 
 	let token,
 		accounts,
-		deployer
+		deployer,
+		receiver
 
 	beforeEach(async () => {
 		//Fetch Token from blockchain
 		const Token = await ethers.getContractFactory("Token")
-		token = await Token.deploy("My Token", "EMR", "1000000")
+		token = await Token.deploy("My Token",
+		"EMR",
+		"1000000")
 
 		accounts = await ethers.getSigners()
 		deployer = accounts[0]
+		receiver = accounts[1]
 	})
 
 	describe("Deployment", () =>{
@@ -48,13 +52,65 @@ describe("Token", () => {
 			expect(await token.totalSupply()).to.equal(totalSupply)
 		})
 
-		it("Has correct deployer", async () => {
-			//Check that the deployer is correct
-			expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
+		it("Assigns total supply to deployer", async () => {
+			//Check that the total supply of the deployer is correct
+			expect(await token.balanceOf(deployer.address)).
+			to.equal(totalSupply)
 		})
-
 
 	})
 
+	describe("Sending tokens", () =>{
 
+		let amount,
+		transaction,
+		result
+
+		describe("Success", () => {
+
+			beforeEach( async () => {
+
+				amount = tokens(100)
+				transaction = await token.connect(deployer).
+				transfer(receiver.address, amount)
+				result = await transaction.wait()
+			})
+
+			it("transfer token balances", async () => {
+				//Transfer tokens
+				expect(await token.balanceOf(deployer.address)).
+				to.equal(tokens(999900))
+				expect(await token.balanceOf(receiver.address)).
+				to.equal(amount)
+			})
+
+			it("emits a Transfer event", async () => {
+				//Check the event
+				const event = result.events[0]
+				expect(event.event).to.equal("Transfer")
+
+				const args = event.args
+				expect(args.from).to.equal(deployer.address)
+				expect(args.to).to.equal(receiver.address)
+				expect(args.value).to.equal(amount)
+			})
+		})
+
+		describe("Failure", () => {
+
+			it("reject insufficient balance", async () => {
+				//Transfer more tokens than the deplyer has
+				const invalidAmount = tokens(10000000)
+				await expect(token.connect(deployer).
+				transfer(receiver.address, invalidAmount)).to.be.reverted
+			})
+
+			it("reject invalid address", async () => {
+				//Doesn't allow to burn tokens
+				await expect(token.connect(deployer).
+				transfer("0x0000000000000000000000000000000000000000", amount))
+				.to.be.reverted
+			})
+		})
+	})
 })
