fb37d9143e378878d6a710d06210c6d807bb6f98
AFPedreros <felipe.pedreros94@gmail.com>
2024-01-29 07:49:11 -0500

    Update biome.json and header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,32 @@
 "use client";
 
 import { useEffect } from "react";
-import { ethers } from "ethers";
+import { Contract, ethers } from "ethers";
+import config from "@/lib/config.json";
+import Token from "~/artifacts/contracts/Token.sol/Token.json";
 
-type Props = {};
+const TOKEN_ABI = Token.abi;
+const TOKEN_ADDRESS = config[31337].CoffeeToken.address;
 
-export default function Header({}: Props) {
+export default function Header() {
   async function loadBlockchainData() {
-    const accounts = await window.ethereum.request({
-      method: "eth_requestAccounts",
-    });
-    console.log(accounts[0]);
+    try {
+      const accounts = await (window as any).ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log(accounts[0]);
 
-    const provider = new ethers.BrowserProvider(window.ethereum);
-    const network = await provider.getNetwork();
-    console.log(network);
+      const provider = new ethers.BrowserProvider((window as any).ethereum);
+      const signer = await provider.getSigner();
+
+      const token = new Contract(TOKEN_ADDRESS, TOKEN_ABI, provider);
+
+      const tokenAddress = await token.getAddress();
+      const symbol = await token.name();
+      console.log("[TOKEN]", tokenAddress, symbol);
+    } catch (error) {
+      console.error("Error in blockchain data loading", error);
+    }
   }
 
   useEffect(() => {
